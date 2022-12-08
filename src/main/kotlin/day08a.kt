e1da3d4899b7b7217e540b8bba8ad6e38e5d351c
Magnus Olsson <fox@fojji.se>
2022-12-08 08:29:58 +0100

    Cleanup 8a

--- src/main/kotlin/day08a.kt
+++ src/main/kotlin/day08a.kt
@@ -1,63 +1,23 @@
 fun main() {
     fun visible(grid: List<String>, x: Int, y: Int): Int {
-        val height = grid[y][x].digitToInt()
+        val height = grid[y][x]
+        val ymax = grid.lastIndex
+        val xmax = grid[0].lastIndex
 
-        // from left
-        var visibleFromLeft = true
-        for (i in 0 until x) {
-            if (grid[y][i].digitToInt() >= height) {
-                println("x=$x, y=$y, i=$i, height=$height is visible from left (${grid[y][i]})")
-                visibleFromLeft = false
-                break
-            }
-        }
-
-        // from right
-        var visibleFromRight = true
-        for (i in x+1 until grid[y].length) {
-            if (grid[y][i].digitToInt() >= height) {
-                println("x=$x, y=$y, i=$i, height=$height is visible from right (${grid[y][i]})")
-                visibleFromRight = false
-                break
-            }
-        }
-        // from top
-        var visibleFromTop = true
-        for (i in 0 until y) {
-            if (grid[i][x].digitToInt() >= height) {
-                println("x=$x, y=$y, i=$i, height=$height is visible from top (${grid[i][x]})")
-                visibleFromTop = false
-                break
-            }
-        }
-        // from bottom
-        var visibleFromBottom = true
-        for (i in y+1 until grid.size) {
-            if (grid[i][x].digitToInt() >= height) {
-                println("x=$x, y=$y, i=$i, height=$height is visible from bottom (${grid[i][x]})")
-                visibleFromBottom = false
-                break
-            }
-        }
-
-
-        if (listOf(visibleFromLeft, visibleFromRight, visibleFromTop, visibleFromBottom).count { it } > 0)
-//        if (sidesVisibleFrom > 0)
-            return 1
-        else
-            return 0
-//        return sidesVisibleFrom
+        val l = (0 until x).all { grid[y][it] < height }
+        val r = (x+1..xmax).all { grid[y][it] < height }
+        val t = (0 until y).all { grid[it][x] < height }
+        val b = (y+1..ymax).all { grid[it][x] < height }
+        return if (l || r || t || b) 1 else 0
     }
 
-    var count = 0
     val lines = getInput("8").lines()
+    var part1 = lines.lastIndex * 4 // perimeter
     for (y in 1 until lines.lastIndex) {
         for (x in 1 until lines[y].lastIndex) {
-            count += visible(lines, x, y)
+            part1 += visible(lines, x, y)
         }
     }
-    count += (lines[0].length-1) * 2
-    count += (lines.size-1) * 2
-    println("count = ${count}")
-    // 1736
+
+    require(part1 == 1736)
 }
